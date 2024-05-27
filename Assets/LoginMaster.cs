e46f209d3f4441d0bb8ce14b52de30648226dfdb
corcho08 <santicorchu@gmail.com>
2024-05-27 14:15:59 -0300

    Cmbios 2

--- Assets/LoginMaster.cs
+++ Assets/LoginMaster.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputField inputfield;
+    private string contras = "holi";
 
-    // Update is called once per frame
-    void Update()
+    public void Boton()
     {
-        
+        if (inputfield.text == contras)
+        {
+            Debug.Log("Access garanted");
+        }
+
+        else
+        {
+            Debug.Log("Access denied"); 
+        }
     }
 }
